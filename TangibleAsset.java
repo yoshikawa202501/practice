a85ca98f60fe33bbb670cd1025ff2c107eceb3ee
yoshikawa202501 <shizukushizuki@yahoo.co.jp>
2025-01-27 13:34:58 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,13 @@
+public abstract class TangibleAsset{
+  private String name;
+  private int price;
+  private String color;
+  public TangibleAsset(String name,int price,String color){
+    this.nama=name;
+    this.price=price;
+    this.color=colar;
+  }
+  public String getName(){return this.name;}
+  public int getPrice(){return this.price;}
+  public String getColor(){return this.color;}
+}
